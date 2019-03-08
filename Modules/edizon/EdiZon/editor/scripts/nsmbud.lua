4348abf94b71d34e021c356339114de9afff0381
tumGER <25822956+tumGER@users.noreply.github.com>
2019-03-09 00:38:18 +0100

    Update EdiZon

--- Modules/edizon/EdiZon/editor/scripts/nsmbud.lua
+++ Modules/edizon/EdiZon/editor/scripts/nsmbud.lua
@@ -4,7 +4,31 @@
 
 checksum = require("lib.checksum")
 
+struct_start = 0x10
+quick_struct_start = 0xC40
+struct_size = 0x208
+offset_pipes = 0x21
+offset_no_crash = 0x51
+offset_unlock_nabbit = 0x4D
 saveFileBuffer = edizon.getSaveFileBuffer()
+
+	
+	for slot_num = 0, 5 do
+		if saveFileBuffer[quick_struct_start + 1 + slot_num * struct_size] > 0 then
+			for i = 0, struct_size do
+				saveFileBuffer[i + struct_start + 1 + slot_num * struct_size] = saveFileBuffer[i + quick_struct_start + 1 + slot_num * struct_size]
+				saveFileBuffer[i + quick_struct_start + 1 + slot_num * struct_size] = 0
+			end
+		
+			saveFileBuffer[quick_struct_start + struct_size - 3 + slot_num * struct_size] = 0xAC
+			saveFileBuffer[quick_struct_start + struct_size - 2 + slot_num * struct_size] = 0x72
+			saveFileBuffer[quick_struct_start + struct_size - 1 + slot_num * struct_size] = 0x7B
+			saveFileBuffer[quick_struct_start + struct_size + slot_num * struct_size] = 0x17
+		end
+	end
+	
+	
+
 
 function getValueFromSaveFile()
 	strArgs = edizon.getStrArgs()
@@ -16,9 +40,8 @@ function getValueFromSaveFile()
 	
 	value = 0
 	
-	struct_start = 0x10
-	struct_size = 0x207
 
+	
 		
 	for i = 0, valueSize - 1 do
 		value = value | (saveFileBuffer[struct_start + offset + i + 1 + slot * struct_size] << i * 8)
@@ -31,17 +54,51 @@ end
 function StarCoins()
 	strArgs = edizon.getStrArgs()
 	slot = tonumber(strArgs[1], 16)
-	offsetStarCoins = tonumber(strArgs[2], 16)
+	offset = tonumber(strArgs[2], 16)
 
 	for i = 0, 40 do
-		saveFileBuffer[i + 1 + offsetStarCoins + 0x10 + 0x207 * slot] = 119
+		saveFileBuffer[i + 1 + offset + struct_start + struct_size * slot] = 119
 	end
 	
 	
 			
-	return "All Star Coins unlocked"
+	return "Star Coins unlocked"
 end
 
+function UnlockLevels()
+	strArgs = edizon.getStrArgs()
+	slot = tonumber(strArgs[1], 16)
+	offset = tonumber(strArgs[2], 16)
+	
+	lvl_array = {0x47, 0xCF, 0x47, 0x47, 0x47, 0x1, 0x1, 0x1, 0, 0, 0, 0x47, 0x47, 0x47, 0x47, 0x47, 0x47, 0xCF, 0x47, 0x47, 0x1, 0x1, 0x1, 0x42, 0, 0x47, 0x47, 0x47, 0x47, 0x47, 0x47, 0x47, 0x47, 0x1, 0x1, 0x1, 0, 0, 0x47, 0xCF, 0x47, 0x47, 0x47, 0x47, 0x47, 0x47, 0x47, 0x1, 0x1, 0x1, 0, 0, 0, 0x42, 0x47, 0xCF, 0x47, 0x47, 0x47, 0xCF, 0x47, 0xCF, 0xCF, 0x47, 0x47, 0x1, 0x1, 0x1, 0, 0x42, 0, 0x47, 0xCF, 0x47, 0x47, 0x47, 0x47, 0x47, 0x47, 0x47, 0xCF, 0x47, 0x47, 0x1, 0x1, 0x1, 0xCF, 0x47, 0xCF, 0x47, 0x47, 0x47, 0x47, 0x47, 0x47, 0x1, 0x1, 0x1, 0, 0, 0xCF, 0x47, 0x47, 0x47, 0xCF, 0x47, 0x47, 0x47, 0x47, 0x47, 0x47, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1}
+	
+	for i = 1, #lvl_array do
+		if (saveFileBuffer[i + offset + struct_start + struct_size * slot] < 0x42) or (lvl_array[i] == 0xCF and saveFileBuffer[i + offset + struct_start + struct_size * slot] == 1) then 
+			saveFileBuffer[i + offset + struct_start + struct_size * slot] = lvl_array[i]
+		elseif (saveFileBuffer[i + offset + struct_start + struct_size * slot] == 0x43) and (lvl_array[i] == 0xCF) then
+			saveFileBuffer[i + offset + struct_start + struct_size * slot] = 0xC3
+		end
+	end
+
+	
+	if slot >=3 then
+		saveFileBuffer[1 + struct_start + struct_size * slot + offset_pipes - 1] = 0xFF
+	else
+		saveFileBuffer[1 + struct_start + struct_size * slot + offset_pipes] = 0xFF
+	end
+	
+	
+	if saveFileBuffer[1 + struct_start + struct_size * slot + offset_no_crash] < 3 then
+		saveFileBuffer[1 + struct_start + struct_size * slot + offset_no_crash] = 0x03
+	end
+	
+	if saveFileBuffer[1 + struct_start + struct_size * slot + offset_unlock_nabbit] == 0xFF then
+		saveFileBuffer[1 + struct_start + struct_size * slot + offset_unlock_nabbit] = 0x00
+		saveFileBuffer[1 + struct_start + struct_size * slot + offset_unlock_nabbit + 1] = 0x00
+	end
+			
+	return "Levels unlocked"
+end
 
 function setValueInSaveFile(value)
 	strArgs = edizon.getStrArgs()
@@ -53,8 +110,6 @@ function setValueInSaveFile(value)
 	addressSize = intArgs[1]
 	valueSize = intArgs[2]
 	
-	struct_start = 0x10
-	struct_size = 0x207
 	
 
 	if dummy == "Lives" then
@@ -69,6 +124,7 @@ function setValueInSaveFile(value)
 	
 end
 
+
 function copyquickslot()
     s1=0x10
 	s2=0x218
@@ -77,70 +133,48 @@ function copyquickslot()
 	qs2=0xE48
 	qs3=0x1050
 	
-	if saveFileBuffer[qs1+1] > 0 then
-		for i = 0, struct_size - 3 do
-			saveFileBuffer[i + s1 + 1] = saveFileBuffer[i + qs1 + 1]
-			saveFileBuffer[i + qs1 + 1] = 0
-		end
+	for slot_num = 0, 5 do
+		if saveFileBuffer[qs1 + 1 + slot_num * struct_size] > 0 then
+			for i = 0, struct_size do
+				saveFileBuffer[i + s1 + 1 + slot_num * struct_size] = saveFileBuffer[i + qs1 + 1 + slot_num * struct_size]
+				saveFileBuffer[i + qs1 + 1 + slot_num * struct_size] = 0
+			end
 		
-		saveFileBuffer[qs1 + struct_size - 2] = 0xAC
-		saveFileBuffer[qs1 + struct_size - 1] = 0x72
-		saveFileBuffer[qs1 + struct_size] = 0x7B
-		saveFileBuffer[qs1 + struct_size + 1] = 0x17
+			saveFileBuffer[qs1 + struct_size - 3 + slot_num * struct_size] = 0xAC
+			saveFileBuffer[qs1 + struct_size - 2 + slot_num * struct_size] = 0x72
+			saveFileBuffer[qs1 + struct_size - 1 + slot_num * struct_size] = 0x7B
+			saveFileBuffer[qs1 + struct_size + slot_num * struct_size] = 0x17
+		end
 	end
 	
-	if saveFileBuffer[qs2+1] > 0 then
-		for i = 0, struct_size - 3 do
-			saveFileBuffer[i + s2 + 1] = saveFileBuffer[i + qs2 + 1]
-			saveFileBuffer[i + qs2 + 1] = 0
-		end
-		
-		saveFileBuffer[qs2 + struct_size - 2] = 0xAC
-		saveFileBuffer[qs2 + struct_size - 1] = 0x72
-		saveFileBuffer[qs2 + struct_size] = 0x7B
-		saveFileBuffer[qs2 + struct_size + 1] = 0x17
-	end
-	
-	if saveFileBuffer[qs3+1] > 0 then
-		for i = 0, struct_size - 3 do
-			saveFileBuffer[i + s3 + 1] = saveFileBuffer[i + qs3 + 1]
-			saveFileBuffer[i + qs3 + 1] = 0
-		end
-		
-		saveFileBuffer[qs3 + struct_size - 2] = 0xAC
-		saveFileBuffer[qs3 + struct_size - 1] = 0x72
-		saveFileBuffer[qs3 + struct_size] = 0x7B
-		saveFileBuffer[qs3 + struct_size + 1] = 0x17
-	end
+
 end
 
 
 function setChecksum()
 	gameFileBuffer = {}
 	
-	struct_start = 0x10
-	struct_size = 0x207
 	
 	
-	for num_struct = 0,2 do 
+	for num_struct = 0,5 do 
 	address = 1
 	
-		for i = 1 + struct_start + num_struct * struct_size + num_struct, 1 + struct_start + struct_size * (num_struct + 1) - 4 + num_struct do
-			gameFileBuffer[address] = saveFileBuffer[i]
+		for i = 1 + struct_start + num_struct * struct_size,  struct_start + (struct_size * (num_struct + 1)) - 4 do
+			gameFileBuffer[address] = saveFileBuffer[i]			
 			address = address + 1
 		end
 		
 			crc = checksum.crc32(string.char(table.unpack(gameFileBuffer)))
    
 		for i = 0, 3 do                                           
-			saveFileBuffer[i + 1 + struct_start + struct_size * (num_struct + 1) - 3 + num_struct] = (crc & (0xFF000000 >> (i * 8))) >> (24 - i * 8)
+			saveFileBuffer[i + struct_start + struct_size * (num_struct + 1) - 3 ] = (crc & (0xFF000000 >> (i * 8))) >> (24 - i * 8)
 		end
 	end 
 	
 end
 
 function getModifiedSaveFile()
-	copyquickslot()
+	--copyquickslot()
 	setChecksum()
 	return saveFileBuffer
 end
