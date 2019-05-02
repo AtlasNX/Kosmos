35de086a2d7f58725dc7bafadabd4ef58a25795c
noahc3 <noahcuroe@gmail.com>
2019-05-02 13:34:08 -0500

    Use output directory variable

--- Modules/merger.py
+++ Modules/merger.py
@@ -12,7 +12,7 @@ modules = ["appstore", "bootlogo", "edizon", "goldleaf",
         "hbmenu", "hekate_payload", "must_have", "kosmosupdater", 
         "sys-ftpd", "sys-netcheat", "kosmos_toolkit", "emuiibo",
         "lockpick", "hid-mitm", "sys-clk", "ldn_mitm"] # Everything that will be merged together
-p2 = "compiled" # How the merged folder should be called
+output_dir = "compiled" # How the merged folder should be called
 
 print("""
                         https://github.com/AtlasNX/Kosmos
@@ -30,15 +30,15 @@ print("""
 It could be that a permissions error will pop up, fix it by restarting the python script! We don\'t really know why that happens!
 """) # Fancy stuff
 
-if os.path.exists("compiled"):
-    shutil.rmtree("compiled") # Delete Content of "compiled" if it exists!
+if os.path.exists(output_dir):
+    shutil.rmtree(output_dir) # Delete Content of "compiled" if it exists!
 
-os.makedirs("compiled") # Double check
+os.makedirs(output_dir) # Double check
 
 # Copy modules to "compiled" directory
 working_dir = os.path.dirname(os.path.realpath(__file__))
 for path in modules:
     path = os.path.join(working_dir, path)
-    copy_tree(path, "compiled")
+    copy_tree(path, output_dir)
 
 print("Done!")
