951167d98542bce045b63488487353218c4beb44
noahc3 <noahc3@users.noreply.github.com>
2019-05-03 10:10:58 -0500

    Improve merger.py (#217)
    * Add .gitignore
    
    * Use standard library for file copy operations, adds Windows support
    
    * Use output directory variable
    
    * Replace token in all files with passed in Kosmos version

--- Modules/merger.py
+++ Modules/merger.py
@@ -4,13 +4,22 @@
 import subprocess as sbp
 import os
 import shutil
+import sys
 import time
+
+from distutils.dir_util import copy_tree
 
 modules = ["appstore", "bootlogo", "edizon", "goldleaf", 
         "hbmenu", "hekate_payload", "must_have", "kosmosupdater", 
         "sys-ftpd", "sys-netcheat", "kosmos_toolkit", "emuiibo",
         "lockpick", "hid-mitm", "sys-clk", "ldn_mitm"] # Everything that will be merged together
-p2 = "compiled" # How the merged folder should be called
+output_dir = "compiled" # How the merged folder should be called
+
+if len(sys.argv) != 2:
+    print("usage: merger.py <kosmos_version>")
+    exit()
+
+version = sys.argv[1]
 
 print("""
                         https://github.com/AtlasNX/Kosmos
@@ -28,17 +37,25 @@ print("""
 It could be that a permissions error will pop up, fix it by restarting the python script! We don\'t really know why that happens!
 """) # Fancy stuff
 
-if os.path.exists("compiled"):
-    shutil.rmtree("compiled") # Delete Content of "compiled" if it exists!
+if os.path.exists(output_dir):
+    shutil.rmtree(output_dir) # Delete Content of "compiled" if it exists!
 
-os.makedirs("compiled") # Double check
+os.makedirs(output_dir) # Double check
 
+# Copy modules to "compiled" directory
+working_dir = os.path.dirname(os.path.realpath(__file__))
 for path in modules:
-    fol = os.listdir(path)
-    for i in fol:
-        p1 = os.path.join(path,i)
-        p3 = 'cp -r ' + p1 +' ' + p2+'/.'
-        sbp.Popen(p3,shell=True)
-        time.sleep(0.1)
+    path = os.path.join(working_dir, path)
+    copy_tree(path, output_dir)
+
+# Check every file in the output directory for the {$KOSMOS_VERSION} token and replace it with
+# the supplied Kosmos version.
+for directory, subdirectories, files in os.walk(output_dir):
+    for file in files:
+        contents = open(os.path.join(directory, file)).read()
+        contents = contents.replace("{$KOSMOS_VERSION}", version)
+        handle = open(os.path.join(directory, file), "w")
+        handle.write(contents)
+        handle.close()
 
 print("Done!")
