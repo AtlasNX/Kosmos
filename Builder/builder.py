fbc1c4ccb5ab73be78a16d19540d566213eee256
Nichole Mattera <697668+NicholeMattera@users.noreply.github.com>
2020-02-28 22:04:17 -0500

    Added license. Switched to argparse. Added JSON file of modules.

--- Builder/builder.py
+++ Builder/builder.py
@@ -1,145 +1,157 @@
 #!/usr/bin/env python
-"""Kosmos Builder v3.0.0
+#
+# Kosmos Builder
+# Copyright (C) 2020 Nichole Mattera
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+# 02110-1301, USA.
+#
 
-Usage:
-    builder.py kosmos <output> [--version=<version>]
-    builder.py sdsetup <output> [--version=<version>] [-a | --auto]
-    builder.py (-h | --help)
-
-Options:
-    -h --help               Show this screen.
-    --version=<version>     Overides the Kosmos Version from the config file.
-    -a --auto               Perform an auto build.
-
-"""
-
+import argparse
 import config
-from docopt import docopt
-from modules import Modules
+import enum
+import json
+import modules
 import os
 import uuid
 import shutil
 
-def createTempDirectory():
-    directory = f"{os.getcwd()}/tmp/{str(uuid.uuid4())}"
+
+class Command(enum.Enum):
+    Kosmos = 0
+    SDSetup = 1
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-v', '--version',
+        default=None,
+        type=str,
+        help='Overides the Kosmos Version from the config file.',
+        metavar='KosmosVersion')
+    subparsers = parser.add_subparsers()
+
+    # Kosmos subcommands
+    parser_kosmos = subparsers.add_parser(
+        'kosmos',
+        help='Create a release build of Kosmos.')
+    parser_kosmos.add_argument(
+        'output',
+        help='Zip file to create.')
+    parser_kosmos.set_defaults(command=Command.Kosmos)
+
+    # SDSetup subcommands
+    parser_sdsetup = subparsers.add_parser(
+        'sdsetup',
+        help='Create a Kosmos modules for SDSetup.')
+    parser_sdsetup.add_argument(
+        'output',
+        help='Directory to output modules to.')
+    parser_sdsetup.add_argument(
+        '-a', '--auto',
+        action='store_true',
+        default=False,
+        help='Perform an auto build.')
+    parser_sdsetup.set_defaults(command=Command.SDSetup)
+
+    # Parse arguments
+    return parser.parse_args()
+
+def create_temp_directory():
+    directory = f'{os.getcwd()}/tmp/{str(uuid.uuid4())}'
     os.makedirs(directory)
     return directory
 
-def getKosmosVersion(args):
-    if args["--version"] is not None:
-        return args["--version"]
+def get_kosmos_version(args):
+    if args.version is not None:
+        return args.version
     return config.version
 
-def initVersionMessages(args):
-    if args["kosmos"]:
-        return [ f"Kosmos {kosmos_version} built with:" ]
-    elif args["sdsetup"] and not args["--auto"]:
-        return [ "SDSetup Modules built with:" ]
+def init_version_messages(args, kosmos_version):
+    if args.command == Command.Kosmos:
+        return [ f'Kosmos {kosmos_version} built with:' ]
+    elif args.command == Command.SDSetup and not args.auto:
+        return [ 'SDSetup Modules built with:' ]
     return []
 
-def deleteFileOrFolder(path):
+def build(args, temp_directory, kosmos_version):
+    results = []
+
+    # Open up modules.json
+    with open('modules.json') as json_file:
+        # Parse JSON
+        data = json.load(json_file)
+
+        # Loop through modules
+        for module in data:
+            # Running a Kosmos Build
+            if args.command == Command.Kosmos:
+                # Download the module.
+                print(f'Downloading {module["name"]}...')
+                download = getattr(modules,
+                    module['download_function_name'])
+                version = download(temp_directory, kosmos_version)
+                results.append(f'  {module["name"]} - {version}')
+
+            # Running a SDSetup Build
+            elif args.command == Command.SDSetup and module['include_in_sdsetup']:
+                # Only show prompts when it's not an auto build.
+                if not args.auto:
+                    print(f'Downloading {module["name"]}...')
+
+                # Download the module.
+                version = ''
+                if not args.auto or (
+                    args.auto and module['include_in_sdsetup_auto_builds']):
+                    download = getattr(modules,
+                        module['download_function_name'])
+                    version = download(temp_directory, kosmos_version)
+
+                # Auto builds have a different prompt at the end for parsing.
+                if args.auto and module['include_in_sdsetup_auto_builds']:
+                    results.append(f'{module["sdsetup_auto_name"]}:{version}')
+                else:
+                    results.append(f'  {module["name"]} - {version}')
+    return results
+
+def delete_file_or_folder(path):
     if os.path.exists(path):
         if os.path.isfile(path):
             os.remove(path)
         else:
             shutil.rmtree(path)
 
-if __name__ == "__main__":
-    args = docopt(__doc__)
+if __name__ == '__main__':
+    args = parse_args()
+    temp_directory = create_temp_directory()
+    kosmos_version = get_kosmos_version(args)
 
-    temp_directory = createTempDirectory()
-    kosmos_version = getKosmosVersion(args)
-    version_messages = initVersionMessages(args)
+    version_messages = init_version_messages(args, kosmos_version)
+    version_messages += build(args, temp_directory, kosmos_version)
 
-    showPrompts = args["kosmos"] or (args["sdsetup"] and not args["--auto"])
+    delete_file_or_folder(args.output)
 
-    if showPrompts:
-        print("Downloading Atmosphere...")
-    Modules.download_atmosphere(temp_directory)
-
-    if showPrompts:
-        print("Downloading Hekate...")
-    Modules.download_hekate(temp_directory, kosmos_version)
-
-    if args["kosmos"] or (args["sdsetup"] and args["--auto"]):
-        if showPrompts:
-            print("Downloading Homebrew App Store...")
-        Modules.download_appstore(temp_directory)
-
-    if args["kosmos"] or (args["sdsetup"] and args["--auto"]):
-        if showPrompts:
-            print("Downloading EdiZon...")
-        Modules.download_edizon(temp_directory)
-
-    if showPrompts:
-        print("Downloading Emuiibo...")
-    Modules.download_emuiibo(temp_directory)
-
-    if args["kosmos"] or (args["sdsetup"] and args["--auto"]):
-        if showPrompts:
-            print("Downloading Goldleaf...")
-        Modules.download_goldleaf(temp_directory)
-
-    if showPrompts:
-        print("Downloading Kosmos Toolbox...")
-    Modules.download_kosmos_toolbox(temp_directory)
-
-    if showPrompts:
-        print("Downloading Kosmos Updater...")
-    Modules.download_kosmos_updater(temp_directory, kosmos_version)
-
-    if showPrompts:
-        print("Downloading ldn_mitm...")
-    Modules.download_ldn_mitm(temp_directory)
-
-    if args["kosmos"] or (args["sdsetup"] and args["--auto"]):
-        if showPrompts:
-            print("Downloading Lockpick...")
-        Modules.download_lockpick(temp_directory)
-
-    if showPrompts:
-        print("Downloading Lockpick_RCM...")
-    Modules.download_lockpick_rcm(temp_directory)
-
-    if args["kosmos"]:
-        print("Downloading nxdumptool...")
-        Modules.download_nxdumptool(temp_directory)
-
-    if args["kosmos"]:
-        print("Downloading nx-ovlloader...")
-        Modules.download_nx_ovlloader(temp_directory)
-
-    if args["kosmos"]:
-        print("Downloading ovlSysModules...")
-        Modules.download_ovl_sysmodules(temp_directory)
-
-    if showPrompts:
-        print("Downloading sys-clk...")
-    Modules.download_sys_clk(temp_directory)
-
-    if showPrompts:
-        print("Downloading sys-con...")
-    Modules.download_sys_con(temp_directory)
-
-    if showPrompts:
-        print("Downloading sys-ftpd-light...")
-    Modules.download_sys_ftpd_light(temp_directory)
-
-    if args["kosmos"]:
-        print("Downloading Tesla-Menu...")
-        Modules.download_tesla_menu(temp_directory)
-
-    deleteFileOrFolder(args["<output>"])
-
-    if args["kosmos"]:
+    if args.command == Command.Kosmos:
         shutil.make_archive(
-            os.path.splitext(args["<output>"])[0],
-            "zip",
+            os.path.splitext(args.output)[0],
+            'zip',
             temp_directory)
-    elif args["sdsetup"]:
-        print("TODO")
+    elif args.command == Command.SDSetup:
+        shutil.move(temp_directory, args.output)
 
-    deleteFileOrFolder(f"{os.getcwd()}/tmp")
+    delete_file_or_folder(f'{os.getcwd()}/tmp')
 
     for message in version_messages:
         print(message)
--- Builder/modules.py
+++ Builder/modules.py
@@ -1,73 +1,74 @@
-class Modules():
+#
+# Kosmos Builder
+# Copyright (C) 2020 Nichole Mattera
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+# 02110-1301, USA.
+#
 
-    @staticmethod
-    def download_atmosphere(temp_directory):
-        print("TODO")
 
-    @staticmethod
-    def download_hekate(temp_directory, kosmos_version):
-        print("TODO")
+def download_atmosphere(temp_directory, kosmos_version):
+    return 'v1.0.0'
 
-    @staticmethod
-    def download_appstore(temp_directory):
-        print("TODO")
+def download_hekate(temp_directory, kosmos_version):
+    return 'v1.0.1'
 
-    @staticmethod
-    def download_edizon(temp_directory):
-        print("TODO")
+def download_appstore(temp_directory, kosmos_version):
+    return 'v1.0.2'
 
-    @staticmethod
-    def download_emuiibo(temp_directory):
-        print("TODO")
+def download_edizon(temp_directory, kosmos_version):
+    return 'v1.0.3'
 
-    @staticmethod
-    def download_goldleaf(temp_directory):
-        print("TODO")
+def download_emuiibo(temp_directory, kosmos_version):
+    return 'v1.0.4'
 
-    @staticmethod
-    def download_kosmos_toolbox(temp_directory):
-        print("TODO")
+def download_goldleaf(temp_directory, kosmos_version):
+    return 'v1.0.5'
 
-    @staticmethod
-    def download_kosmos_updater(temp_directory, kosmos_version):
-        print("TODO")
+def download_kosmos_toolbox(temp_directory, kosmos_version):
+    return 'v1.0.6'
 
-    @staticmethod
-    def download_ldn_mitm(temp_directory):
-        print("TODO")
+def download_kosmos_updater(temp_directory, kosmos_version):
+    return 'v1.0.7'
 
-    @staticmethod
-    def download_lockpick(temp_directory):
-        print("TODO")
+def download_ldn_mitm(temp_directory, kosmos_version):
+    return 'v1.0.8'
 
-    @staticmethod
-    def download_lockpick_rcm(temp_directory):
-        print("TODO")
+def download_lockpick(temp_directory, kosmos_version):
+    return 'v1.0.9'
 
-    @staticmethod
-    def download_nxdumptool(temp_directory):
-        print("TODO")
+def download_lockpick_rcm(temp_directory, kosmos_version):
+    return 'v1.1.0'
 
-    @staticmethod
-    def download_nx_ovlloader(temp_directory):
-        print("TODO")
+def download_nxdumptool(temp_directory, kosmos_version):
+    return 'v1.1.1'
 
-    @staticmethod
-    def download_ovl_sysmodules(temp_directory):
-        print("TODO")
+def download_nx_ovlloader(temp_directory, kosmos_version):
+    return 'v1.1.2'
 
-    @staticmethod
-    def download_sys_clk(temp_directory):
-        print("TODO")
+def download_ovl_sysmodules(temp_directory, kosmos_version):
+    return 'v1.1.3'
 
-    @staticmethod
-    def download_sys_con(temp_directory):
-        print("TODO")
+def download_sys_clk(temp_directory, kosmos_version):
+    return 'v1.1.4'
 
-    @staticmethod
-    def download_sys_ftpd_light(temp_directory):
-        print("TODO")
+def download_sys_con(temp_directory, kosmos_version):
+    return 'v1.1.5'
 
-    @staticmethod
-    def download_tesla_menu(temp_directory):
-        print("TODO")
+def download_sys_ftpd_light(temp_directory, kosmos_version):
+    return 'v1.1.6'
+
+def download_tesla_menu(temp_directory, kosmos_version):
+    return 'v1.1.7'
